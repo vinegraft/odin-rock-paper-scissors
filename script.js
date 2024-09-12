01c13792e3ef2c6ef5fbfe67dc1f88e757879c9e
vinegraft <982042+vinegraft@users.noreply.github.com>
2024-09-12 12:42:26 -0400

    Add logic for player's choice of rock, paper or scissors

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const log = console.log;
+console.clear();
+let playerChoice = "";
+let computerChoice = "";
+
 function getComputerChoice() {
   let random = Math.random() * 3;
   let choice = "";
@@ -13,3 +17,24 @@ function getComputerChoice() {
   return choice;
 }
 log(getComputerChoice());
+
+function getPlayerChoice() {
+  let choice = "";
+  choice = prompt("Enter your choice between Rock, Paper or Scissors");
+
+  if (
+    choice != null &&
+    (choice.toLowerCase() === "rock" ||
+      choice.toLowerCase() === "paper" ||
+      choice.toLowerCase() === "scissors")
+  ) {
+    playerChoice = choice.toLowerCase();
+  } else {
+    alert("Enter a valid choice!");
+    getPlayerChoice();
+  }
+  return playerChoice;
+}
+playerChoice = getPlayerChoice();
+
+log(playerChoice);
